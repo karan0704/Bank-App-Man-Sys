c6986e70f0132152f4774c8b7dfd11059ad9ad72
Karan Mohokar <karanmohokar7@gmail.com>
2024-02-14 22:16:01 +0530

    Basic Code added to Service Layer

--- src/controller/Admin.java
+++ src/controller/Admin.java
@@ -1,7 +1,36 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Admin {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.print("Welcome to Bank");
+			System.out.print("Select option for Banking");
+			System.out.print("1. Create Account"
+					+ "2. Check Balance"
+					+ "3. Withdraw Money"
+					+ "4. Add Money");
+			
+			int bankingOption = sc.nextInt();
+			
+			switch (bankingOption) {
+			case 1:
+				
+				break;
+
+			default:
+				break;
+			}
+			
+			sc.close();
+			
+			
+			
+			
+		} while (true);
 		
 	}
 
--- src/service/SBI.java
+++ src/service/SBI.java
@@ -2,4 +2,28 @@ package service;
 
 public class SBI implements RBI{
 
+	@Override
+	public void createAccount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkBalance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdrawMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
