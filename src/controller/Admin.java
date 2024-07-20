dc52c8d14dbc585819af17423fcdaf62b92fc49b
Karan Mohokar <karanmohokar7@gmail.com>
2024-07-20 21:00:49 +0530

    Changes Added

--- src/controller/Admin.java
+++ src/controller/Admin.java
@@ -7,17 +7,17 @@ import service.RBI;
 import service.SBI;
 
 public class Admin {
-	
+
 	private Scanner sc;
-	
+
 	public Admin(Scanner sc) {
 		this.sc = sc;
 	}
-	
-	public void callMenu() {;
+
+	public void callMenu() {
+		;
 		RBI sbi = new SBI(sc);
-		
-		
+
 		switch (selectedBankingOption()) {
 
 		case 1:
@@ -35,19 +35,18 @@ public class Admin {
 		case 4:
 			sbi.addMoney();
 			break;
-		
+
 		case 5:
-            System.out.println("Exiting...");
-            return;
-            
+			System.out.println("Exiting...");
+			return;
+
 		default:
 			System.out.println("Choose valid option");
 			break;
 		}
 	}
 
-	
-	public int selectedBankingOption() {		
+	public int selectedBankingOption() {
 		System.out.print("Welcome to Bank\n");
 		System.out.print("\nBanking Option");
 		System.out.print("\n1. Create Account" + "\n2. Check Balance" + "\n3. Withdraw Money" + "\n4. Add Money\n"
--- src/service/SBI.java
+++ src/service/SBI.java
@@ -6,70 +6,65 @@ import model.Account;
 
 public class SBI implements RBI {
 
-	private Scanner sc;	
+	private Scanner sc;
 	Account accountObject;
-	 
+
 	public SBI(Scanner sc) {
 		this.sc = sc;
 	}
 
 	@Override
 	public void createAccount() {
-		
+
 		System.out.println("\nWrite your name below\n");
-		 sc.nextLine(); // Consume newline character
-		 Account accountObject = new Account();
+		sc.nextLine(); // Consume newline character
+		accountObject = new Account();
 		String name = sc.nextLine();
 		accountObject.setName(name);
-		
+
 		System.out.println("\nWrite your ID below\t");
 		// idNo = Saving input value
 		int idNo = sc.nextInt();
-		System.out.println("Id No = "+idNo);
+		System.out.println("Id No = " + idNo);
 
 		if (idNo != 0) {
-			System.out.println("(Checking if Id No == 0)"
-					+ "Id No = "+idNo);
-			
+			System.out.println("(Checking if Id No == 0)" + "Id No = " + idNo);
+
 			accountObject.setId(idNo);
-			System.out.println("Id No set = "+idNo);
-			
-			System.out.println("Account ID got -> "+accountObject.getId());
+			System.out.println("Id No set = " + idNo);
+
+			System.out.println("Account ID got -> " + accountObject.getId());
 		}
-		
-		
+
 		System.out.println("\nWrite your Address below\t");
-		 sc.nextLine();
+		sc.nextLine();
 		String add = sc.nextLine();
 		accountObject.setAdd(add);
-		
-		
+
 		System.out.println("\nWrite your starting balance below\t");
 		double startingBalance = sc.nextDouble();
 		sc.nextLine(); // Consume newline character
 		accountObject.setBalance(startingBalance);
-		
+
 		System.out.println("\nAccount is created\n");
 		System.out.println("---------------------------");
-	}	
+	}
 
-// ***********************************Here is the Problem**********************
 	@Override
-		public void checkBalance() {
-		            System.out.println("\nWhat is your ID No\t");
-		            int idNo = sc.nextInt();
-		            System.out.println("Typed Id No is "+idNo);
+	public void checkBalance() {
+		System.out.println("\nWhat is your ID No\t");
+		int idNo = sc.nextInt();
+		System.out.println("Typed Id No is " + idNo);
+		System.out.println("Name -> " + accountObject.getName());
+		if (accountObject != null && accountObject.getId() == idNo) {
+			// System.out.println("Name -> "+accountObject.getName());
+			System.out.println("Balance -> " + accountObject.getBalance());
+		} else {
+			System.out.println("Invalid ID.");
+			System.out.println("Id No = " + idNo);
+		}
+	}
 
-		            if (accountObject.getId() == idNo ) {
-		                System.out.println("Name -> "+accountObject.getName());
-		                System.out.println("Balance -> "+accountObject.getBalance());
-		            } 
-		            else {
-		            	System.out.println("Invalid ID.");
-		                System.out.println("Id No = "+idNo);
-		            }
-			}
-// ***********************************Here is the Problem**********************
 	@Override
 	public void withdrawMoney() {
 		// TODO Auto-generated method stub
@@ -84,14 +79,11 @@ public class SBI implements RBI {
 
 	@Override
 	public int tryAgainExit() {
-		System.out.println("1. Please try again"
-				+ "2. Exit");
-		
+		System.out.println("1. Please try again" + "2. Exit");
+
 		int tryAgainOrExit = sc.nextInt();
 		return tryAgainOrExit;
-		
+
 	}
-	
-	
 
 }
